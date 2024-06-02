a48f0fa02e929308180e0c924c7713960f93a12a
haraldchan <katahane42@gmail.com>
2024-06-02 16:46:24 +0800

    added batchCheckin experimental lines.

--- src/ActionModules/BatchCheckout/batch-checkout-snippets.js
+++ src/ActionModules/BatchCheckout/batch-checkout-snippets.js
@@ -4,7 +4,7 @@ const querySelect = document.querySelector('input[placeholder="请选择字段"]
 
 const change = new Event('input', {
 	bubbles: true,
-	cancelable: true
+	cancelable: true,
 })
 
 document.querySelectorAll('.el-select-dropdown__item')[3].click()
@@ -28,3 +28,26 @@ queryBtn.click()
 coBtn = Array.from(document.querySelectorAll('span')).find((span) => span.innerText === '退房')
 coBtn.click()
 okBtn.click()
+
+
+
+// batch check in 
+// perhaps I should embbed this to ProfileClipper extension?
+function findSpan(label){
+	return Array.from(document.querySelectorAll('span')).find((span) => span.innerText === label)
+}
+
+// TODO: use setTimeout might not be a good idea, maybe use check display classes of something
+const batchCheckin = setInterval(() => {
+	findSpan('修改').click()
+	findSpan('上报(R)').click()
+	
+	if (findSpan('一同入住')) {
+		findSpan('一同入住').click()
+	}
+
+	if (findSpan('暂无数据')) {
+		console.log('done.')
+		clearInterval(batchCheckin)
+	}
+}, 2000);
